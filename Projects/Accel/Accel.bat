2da5ce03082ed72dfdb2a24b3cdcae2d8d293df2
savageentity <yountnb@gmail.com>
2022-10-01 17:21:15 -0500

    Changing to new Python3 TCP Socket

--- Projects/Accel/Accel.bat
+++ Projects/Accel/Accel.bat
@@ -1,3 +1,3 @@
 echo "STARTING..."
 
-START "C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.10_3.10.2032.0_x64__qbz5n2kfra8p0\python3.10.exe" c:/Users/yount/OneDrive/Desktop/Accel/receiver.py
+START "C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.10_3.10.2032.0_x64__qbz5n2kfra8p0\python3.10.exe" C:\Users\yount\source\repos\Projects\Accel\receiver.py
--- Projects/Accel/receiver.py
+++ Projects/Accel/receiver.py
@@ -1,28 +1,31 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from socket import *
+import socket
 import sys
 import select
 
 host="192.168.1.65"
 port = 5001
-s = socket(AF_INET,SOCK_DGRAM)
-s.bind((host,port))
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+    s.bind((host,port))
+    s.listen()
+    conn, addr = s.accept()
+    #addr = (host,port)
+    buf=1024
 
-addr = (host,port)
-buf=1024
+    conn.sendall(b'\x01')
 
-data,addr = s.recvfrom(buf)
-print ("Received File:",data.strip())
-f = open(data.strip(),'wb')
+    data,addr = s.recvfrom(buf)
+    print ("Received File:",data.strip())
+    f = open(data.strip(),'wb')
 
-data,addr = s.recvfrom(buf)
-try:
-    while(data):
-        f.write(data)
-        s.settimeout(10)
-        data,addr = s.recvfrom(buf)
-except timeout:
-    f.close()
-    s.close()
-    print ("File Downloaded")
+    data,addr = s.recvfrom(buf)
+    try:
+        while(data):
+            f.write(data)
+            s.settimeout(10)
+            data,addr = s.recvfrom(buf)
+    except timeout:
+        f.close()
+        s.close()
+        print ("File Downloaded")
